6fede3d9290298e17af9aced7109ddc883e6dd07
DomingoCastro <domingocastro1998@outlook.com>
2023-02-24 20:09:11 +0100

    Add project files.

---
+++ MvcIdentityRolesEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcIdentityRolesEmpleados.Filters;
+using MvcIdentityRolesEmpleados.Models;
+using MvcIdentityRolesEmpleados.Repositories;
+
+namespace MvcIdentityRolesEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleado repo;
+        public EmpleadosController(RepositoryEmpleado repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Empleado empleado = this.repo.FindEmpleado(id);
+            return View(empleado);
+        }
+        [AuthorizeEmpleados]
+        public IActionResult PerfilEmpleado()
+        {
+            return View();
+        }
+        [AuthorizeEmpleados]
+        public IActionResult CompisCurro()
+        {
+            //DEBEMOS BUSCAR EL CLAIM CON EL DATO DEL DEPARTAMENTO
+            string dato = HttpContext.User.FindFirst("Departamento").Value;
+            int iddepartamento = int.Parse(dato);
+            List<Empleado> empleados = this.repo.GetEmpleadosDepartamento(iddepartamento);
+            return View(empleados);
+        }
+    }
+}
---
+++ MvcIdentityRolesEmpleados/Filters/AuthorizeEmpleadosAttribute.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MvcIdentityRolesEmpleados.Filters
+{
+    public class AuthorizeEmpleadosAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+            if (user.Identity.IsAuthenticated == false )
+            {
+                context.Result = this.GetRoute("Manager", "Login");
+            }
+            else
+            {
+                //CONTROLLER QUE A REALIZADO LA LLAMADA
+                //string controller = context.RouteData.Values["controller"].ToString();
+                //string action = context.RouteData.Values["action"].ToString();
+                ////HACEMOS UN IF Y SI ES EMPLEADO Y COMPIS CURRO...
+                //if(user.IsInRole("DIRECTOR") == false && controller == "Empleados" && action == "CompisCurro")
+                //{
+                //    context.Result = this.GetRoute("Manager", "ErrorAcceso");
+                //}
+                //VALIDAMOS EL ROLE CON SU OFICIO
+                if(user.IsInRole("DIRECTOR") == false && user.IsInRole("PRESIDENTE") == false)
+                {
+                    context.Result = this.GetRoute("Manager", "ErrorAcceso");
+                }
+            }
+        }
+        //CREAMOS UN METODO DE AYUDA POR SI REDIRECCIONAMOS A MAS SITIOS ADEMAS DE LOGIN
+        private RedirectToRouteResult GetRoute(string controlador, string vista)
+        {
+            RouteValueDictionary ruta = new RouteValueDictionary(new
+            {
+                controller = controlador,
+                action = vista
+            });
+            return new RedirectToRouteResult(ruta);
+        }
+    }
+}
